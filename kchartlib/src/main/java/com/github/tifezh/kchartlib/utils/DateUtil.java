1ba09fc58586f2adb7e5757f50db11858c66904c
yantao.pu <12345678>
2018-11-15 14:37:45 +0800

    LEM

--- kchartlib/src/main/java/com/github/tifezh/kchartlib/utils/DateUtil.java
+++ kchartlib/src/main/java/com/github/tifezh/kchartlib/utils/DateUtil.java
@@ -13,7 +13,7 @@ import java.util.Locale;
  * Description 时间工具类
  * Author puyantao
  * Email 1067899750@qq.com
- * Date 2018-10-26 17:36
+ * Date 2018-11-15 14:37
  */
 
 public class DateUtil {
@@ -21,6 +21,7 @@ public class DateUtil {
     public static SimpleDateFormat shortTimeFormat = new SimpleDateFormat("HH:mm");
     public static SimpleDateFormat shortDateFormat = new SimpleDateFormat("MM/dd");
     public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
     public static String getStringDateByLong(long dateLong, int type) {
         String template = null;
         switch (type) {
@@ -67,6 +68,48 @@ public class DateUtil {
         } catch (ParseException e) {
         }
         return date;
+    }
+
+    public static long dateToStamp(String s, int type) {
+        String template = null;
+        switch (type) {
+            case 1:
+                template = "yyyy-MM-dd HH:mm";
+                break;
+            case 2:
+                template = "yyyy/MM/dd";
+                break;
+            case 3:
+                template = "yyyy年MM月dd日";
+                break;
+            case 4:
+                template = "yyyy-MM-dd";
+                break;
+            case 5:
+                template = "yyyy-MM-dd HH:mm:ss";
+                break;
+            case 6:
+                template = "MM月dd号";
+                break;
+            case 7:
+                template = "yyyy/MM/dd HH:mm:ss";
+                break;
+            case 8:
+                template = "HH:mm";
+                break;
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(template);
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date.getTime();
+    }
+
+    public static void main(String[] argc){
+
     }
 
 }
