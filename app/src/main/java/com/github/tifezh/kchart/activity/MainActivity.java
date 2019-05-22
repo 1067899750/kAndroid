a01a115f0580a3b05e0ba4d16ee2df3042a8c847
puyantao <puyantao123759>
2019-05-22 17:39:51 +0800

    优化

--- app/src/main/java/com/github/tifezh/kchart/activity/MainActivity.java
+++ app/src/main/java/com/github/tifezh/kchart/activity/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, ScaleActivity.class));
+//        startActivity(new Intent(MainActivity.this, ScaleActivity.class));
 
     }
 
